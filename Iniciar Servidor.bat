20da455a658e7feed0a23c0585609dd27ca3f0a9
drmaluendaia-maker <dr.maluenda.ia@gmail.com>
2025-09-02 07:03:58 -0300

    Update Iniciar Servidor.bat

--- Iniciar Servidor.bat
+++ Iniciar Servidor.bat
@@ -1,9 +1,11 @@
 @echo off
-title Servidor de Triage
+title Servidor de Triage v2
+
+echo Verificando e instalando dependencias (si es necesario)...
+call npm install
 
 echo.
-echo Iniciando servidor...
+echo Iniciando servidor de Triage v2...
 echo.
 
 node servidor.js
-
